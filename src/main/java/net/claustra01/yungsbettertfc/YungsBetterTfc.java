0767bba370bc3dafc583a176ad47595358b91bf7
claustra01 <shinya430.g@gmail.com>
2026-02-09 20:27:01 +0900

    update: mod id

---
+++ src/main/java/net/claustra01/yungsbettertfc/YungsBetterTfc.java
@@ -0,0 +1,12 @@
+package net.claustra01.yungsbettertfc;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(YungsBetterTfc.MODID)
+public final class YungsBetterTfc {
+    public static final String MODID = "yungsbettertfc";
+
+    public YungsBetterTfc() {
+        // Intentionally empty: this mod is data-pack driven (tags only).
+    }
+}
